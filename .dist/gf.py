7997340c6a5e19bb2e7118f7ced974c23d970941
Ruslan <144210785+Rusik2004@users.noreply.github.com>
2023-10-03 15:55:27 +0900

    Delete gf.py

--- .dist/gf.py
+++
@@ -1,8 +0,0 @@
-def double_score(mylist):
-    for k in range(len(mylist)):
-        mylist[k] = 2 * mylist[k] 
-
-if __name__ == '__main__':
-    scorelist = [78, 56, 67, 95, 82]
-    double_score(scorelist)
-    print('scorelist =', scorelist)
